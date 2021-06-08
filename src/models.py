2f0311b3f28d496022bd99e8621cad00695e80e3
nereaaf <81961906+nereaaf@users.noreply.github.com>
2021-06-08 12:53:42 +0200

    Add files via upload

---
+++ src/models.py
@@ -0,0 +1,314 @@
+from __init__ import bd, ma
+
+# MODELOS DE TABLAS
+class Usuarios(bd.Model):
+    __tablename__ = 'usuarios'
+    dni = bd.Column(bd.String(9), primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(45), nullable=False)
+    apellido1 = bd.Column(bd.String(45), nullable=False)
+    apellido2 = bd.Column(bd.String(45), nullable=False)
+    email = bd.Column(bd.String(100), nullable=False)
+    password = bd.Column(bd.String(20), nullable=False)
+   
+    def __init__(self, dni, nombre, apellido1, apellido2, email, password):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido1 = apellido1
+        self.apellido2 = apellido2
+        self.email = email
+        self.password = password
+
+class UsuariosSchema(ma.Schema):
+    class Meta:
+        fields = ('dni', 'nombre', 'apellido1', 'apellido2', 'email', 'password') # hereda todos los campos del modelo Usuarios
+
+# ----------------------------------------------------------------------------------
+
+class Administradores(bd.Model):
+    __tablename__ = 'administradores'
+    # foreign key Usuarios --> dni
+    dni_admin = bd.Column(bd.String(9), bd.ForeignKey('usuarios.dni'), primary_key=True, nullable=False)
+
+    def __init__(self, dni_admin):
+        self.dni_admin = dni_admin
+
+class AdministradoresSchema(ma.Schema):
+    class Meta:
+        field=('dni_admin')
+
+# ----------------------------------------------------------------------------------
+
+class Profesores(bd.Model):
+    __tablename__ = 'profesores'
+    # foreign key Usuarios --> dni
+    dni_profesor = bd.Column(bd.String(9), bd.ForeignKey('usuarios.dni'), primary_key=True, nullable=False)
+
+    def __init__(self, dni_profesor):
+        self.dni_profesor = dni_profesor
+
+class ProfesoresSchema(ma.Schema):
+    class Meta:
+        field=('dni_profesor')
+
+# ----------------------------------------------------------------------------------
+
+class Alumnos(bd.Model):
+    __tablename__ = 'alumnos'
+    # foreign key Usuarios --> dni
+    dni_alumno = bd.Column(bd.String(9), bd.ForeignKey('usuarios.dni'), primary_key=True, nullable=False)
+    tlfno = bd.Column(bd.String(9), nullable=False)
+    fechaNac = bd.Column(bd.String(10), nullable=False)
+    lugarNac = bd.Column(bd.String(30), nullable=False)
+    nacionalidad = bd.Column(bd.String(30), nullable=False)
+    domicilio = bd.Column(bd.String(100), nullable=False)
+    localidad = bd.Column(bd.String(30), nullable=False)
+    cp = bd.Column(bd.String(5), nullable=False)
+    municipio = bd.Column(bd.String(45), nullable=False)
+    provincia = bd.Column(bd.String(45), nullable=False)
+    pais = bd.Column(bd.String(45), nullable=False)
+
+    def __init__(self, dni_alumno, tlfno, fechaNac, lugarNac, nacionalidad, domicilio, localidad, cp, municipio, provincia, pais):
+        self.dni_alumno = dni_alumno
+        self.tlfno = tlfno
+        self.fechaNac = fechaNac
+        self.lugarNac = lugarNac
+        self.nacionalidad = nacionalidad
+        self.domicilio = domicilio
+        self.localidad = localidad
+        self.cp = cp
+        self.municipio = municipio
+        self.provincia = provincia
+        self.pais = pais
+
+class AlumnosSchema(ma.Schema):
+    class Meta:
+        fields=('dni_alumno', 'tlfno', 'fechaNac', 'lugarNac', 'nacionalidad', 'domicilio', 'localidad', 'cp', 'municipio', 'provincia', 'pais')
+
+# ----------------------------------------------------------------------------------
+
+class Familias(bd.Model):
+    __tablename__ = 'familias'
+    id_familia = bd.Column(bd.Integer, primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(45), nullable=False)
+    # referencia a model Ciclos
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def __str__(self):
+        return self.nombre
+
+class FamiliasSchema(ma.Schema):
+    class Meta:
+        fields=('id_familia','nombre')
+
+# ----------------------------------------------------------------------------------
+
+class Ciclos(bd.Model):
+    __tablename__ = 'ciclos'
+    id_ciclo = bd.Column(bd.String(7), primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(60), nullable=False)
+    real_decreto = bd.Column(bd.String(100), nullable=False)
+    decreto = bd.Column(bd.String(100), nullable=False)
+    id_familia = bd.Column(bd.Integer, bd.ForeignKey('familias.id_familia'), nullable=False)
+
+    def __init__(self, id_ciclo, nombre, real_decreto, decreto, id_familia):
+        self.id_ciclo = id_ciclo
+        self.nombre = nombre
+        self.real_decreto = real_decreto
+        self.decreto = decreto
+        self.id_familia = id_familia
+
+class CiclosSchema(ma.Schema):
+    class Meta:
+        fields=('id_ciclo', 'nombre', 'real_decreto', 'decreto', 'id_familia')
+
+# ----------------------------------------------------------------------------------
+
+class Cursos(bd.Model):
+    __tablename__ = 'cursos'
+    id_curso = bd.Column(bd.Integer, primary_key=True, nullable=False)
+    num_curso = bd.Column(bd.String(2), nullable=False)
+    id_ciclo = bd.Column(bd.String(7), bd.ForeignKey('ciclos.id_ciclo'), nullable=False)
+
+    def __init__(self, num_curso, id_ciclo):
+        self.num_curso = num_curso
+        self.id_ciclo = id_ciclo
+
+class CursosSchema(ma.Schema):
+    class Meta:
+        fields=('num_curso', 'id_ciclo')
+
+# ----------------------------------------------------------------------------------
+
+class Centros(bd.Model):
+    __tablename__ = 'centros'
+    cod_centro = bd.Column(bd.String(8), primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(100), nullable=False)
+    titularidad = bd.Column(bd.String(45), nullable=False)
+    direccion = bd.Column(bd.String(100), nullable=False)
+    cp = bd.Column(bd.String(5), nullable=False)
+    localidad = bd.Column(bd.String(30), nullable=False)
+    concejo = bd.Column(bd.String(30), nullable=False)
+    provincia = bd.Column(bd.String(45), nullable=False)
+    tlfno = bd.Column(bd.String(9), nullable=False)
+    fax = bd.Column(bd.String(50), nullable=True)
+    correo = bd.Column(bd.String(45), nullable=False)
+
+    def __init__(self, cod_centro, nombre, titularidad, direccion, cp, localidad, concejo, provincia, tlfno, fax, correo):
+        self.cod_centro = cod_centro
+        self.nombre = nombre
+        self.titularidad = titularidad
+        self.direccion = direccion
+        self.cp = cp
+        self.localidad = localidad
+        self.concejo = concejo
+        self.provincia = provincia
+        self.tlfno = tlfno
+        self.fax = fax
+        self.correo = correo
+
+class CentrosSchema(ma.Schema):
+    class Meta:
+        fields=('cod_centro', 'nombre', 'titularidad', 'direccion', 'cp', 'localidad', 'concejo', 'provincia', 'tlfno', 'fax', 'correo')
+
+# ----------------------------------------------------------------------------------
+
+class Cursos_has_centros(bd.Model):
+    __tablename__ = 'cursos_has_centros'
+    id_curso = bd.Column(bd.Integer, bd.ForeignKey('cursos.id_curso'), primary_key=True, nullable=False)
+    cod_centro = bd.Column(bd.String(8), bd.ForeignKey('centros.cod_centro'), primary_key=True, nullable=False)
+
+    def __init__(self, id_curso, cod_centro):
+        self.id_curso = id_curso
+        self.cod_centro = cod_centro
+
+class Cursos_has_centrosSchema(ma.Schema):
+    class Meta:
+        fields=('id_curso', 'cod_centro')
+
+# ----------------------------------------------------------------------------------
+
+class Modulos(bd.Model):
+    __tablename__ = 'modulos'
+    cod_mod = bd.Column(bd.String(5), primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(45), nullable=False)
+    nombre_abrev = bd.Column(bd.String(7), nullable=False)
+    id_curso = bd.Column(bd.Integer, bd.ForeignKey('cursos.id_curso'), nullable=False)
+
+    def __init__(self, cod_mod, nombre, nombre_abrev, id_curso):
+        self.cod_mod = cod_mod
+        self.nombre = nombre
+        self.nombre_abrev = nombre_abrev
+        self.id_curso = id_curso
+
+class ModulosSchema(ma.Schema):
+    class Meta:
+        fields=('cod_mod', 'nombre', 'nombre_abrev', 'id_curso') 
+
+# ----------------------------------------------------------------------------------
+
+class Regimen(bd.Model):
+    __tablename__ = 'regimen'
+    id_regimen = bd.Column(bd.Integer, primary_key=True, nullable=False)
+    nombre_regimen = bd.Column(bd.String(15), nullable=False)
+
+    def __init__(self, id_regimen, nombre_regimen):
+        self.id_regimen = id_regimen
+        self.nombre_regimen = nombre_regimen
+
+class RegimenSchema(ma.Schema):
+    class Meta:
+        fields=('id_regimen', 'nombre_regimen')
+
+# ----------------------------------------------------------------------------------
+
+class Vias_acceso(bd.Model):
+    __tablename__ = 'vias_acceso'
+    id_via = bd.Column(bd.Integer, primary_key=True, nullable=False)
+    nombre = bd.Column(bd.String(45), nullable=False)
+
+    def __init__(self, id_via, nombre):
+        self.id_via = id_via
+        self.nombre = nombre
+
+class Vias_accesoSchema(ma.Schema):
+    class Meta:
+        fields=('id_via', 'nombre')
+
+# ----------------------------------------------------------------------------------
+
+class Sesiones(bd.Model):
+    __tablename__ = 'sesiones'
+    id_sesion = bd.Column(bd.Integer, primary_key=True, nullable=False)
+    tipo_sesion = bd.Column(bd.String(50), nullable=False)
+    tipo_sesion_abr = bd.Column(bd.String(20), nullable=False)
+    curso = bd.Column(bd.String(2), nullable=False)
+
+    def __init__(self, id_sesion, tipo_sesion, tipo_sesion_abr, curso):
+        self.id_sesion = id_sesion
+        self.tipo_sesion = tipo_sesion
+        self.tipo_sesion_abr = tipo_sesion_abr
+        self.curso = curso
+
+class SesionesSchema(ma.Schema):
+    class Meta:
+        fields=('id_sesion', 'tipo_sesion', 'tipo_sesion_abr', 'curso')
+
+# ----------------------------------------------------------------------------------
+
+class Fechas_evaluacion(bd.Model):
+    __tablename__ = 'fechas_evaluacion'
+    cod_sesion = bd.Column(bd.String(30), primary_key=True, nullable=False)
+    fecha_curso = bd.Column(bd.String(9), nullable=False)
+    fecha_eval = bd.Column(bd.Date, nullable=True)
+    id_sesion = bd.Column(bd.Integer, bd.ForeignKey('sesiones.id_sesion'), nullable=False)
+
+    def __init__(self, cod_sesion, fecha_curso, fecha_eval, id_sesion):
+        self.cod_sesion = cod_sesion
+        self.fecha_curso = fecha_curso
+        self.fecha_eval = fecha_eval
+        self.id_sesion = id_sesion
+
+class Fechas_evaluacionSchema(ma.Schema):
+    class Meta:
+        fields=('cod_sesion', 'fecha_curso', 'fecha_eval', 'id_sesion')
+
+# ----------------------------------------------------------------------------------
+
+class Matriculaciones(bd.Model):
+    __tablename__ = 'matriculaciones'
+    n_expediente = bd.Column(bd.String(12), primary_key=True, nullable=False)
+    alumno_dni = bd.Column(bd.String(9), bd.ForeignKey('alumnos.dni_alumno'), nullable=False)
+    año_academico = bd.Column(bd.String(9), nullable=False)
+    via_acceso = bd.Column(bd.Integer, bd.ForeignKey('vias_acceso.id_via'), nullable=False)
+    regimen_mat = bd.Column(bd.Integer, bd.ForeignKey('regimen.id_regimen'), nullable=False)
+    cod_mod = bd.Column(bd.String(5), bd.ForeignKey('modulos.cod_mod'), nullable=False)
+    nota = bd.Column(bd.Integer, nullable=True)
+    n_convocatoria = bd.Column(bd.Integer, nullable=True)
+    cod_sesion = bd.Column(bd.String(30), bd.ForeignKey('fechas_evaluacion.cod_sesion'), nullable=True)
+    cv_o_aa = bd.Column(bd.String(2), nullable=True)
+    apto_noApto = bd.Column(bd.String(10), nullable=True)
+    superada = bd.Column(bd.Boolean,default=False) #¿? TINYINT(1)
+    
+
+    def __init__(self, n_expediente, alumno_dni, año_academico, via_acceso, regimen_mat, cod_mod, nota, n_convocatoria, cod_sesion, cv_o_aa, apto_noApto, superada):
+        self.n_expediente = n_expediente
+        self.alumno_dni = alumno_dni
+        self.año_academico = año_academico
+        self.via_acceso = via_acceso
+        self.regimen_mat = regimen_mat
+        self.cod_mod = cod_mod
+        self.nota = nota
+        self.n_convocatoria = n_convocatoria
+        self.cod_sesion = cod_sesion
+        self.cv_o_aa = cv_o_aa
+        self.apto_noApto = apto_noApto
+        self.superada = superada
+
+class MatriculacionesSchema(ma.Schema):
+    class Meta:
+        fields=('n_expediente', 'alumno_dni', 'año_academico', 'via_acceso', 'regimen', 'cod_mod', 'nota', 'n_convocatoria', 'cod_sesion', 'cv_o_aa', 'apto_noApto', 'superada')
+
+
+
---
+++ src/routes.py
@@ -0,0 +1,436 @@
+import re
+from models import Familias, FamiliasSchema, UsuariosSchema,Ciclos, CiclosSchema, Regimen, RegimenSchema
+from __init__ import app, bd
+from flask import request, jsonify
+from models import *
+
+#USUARIOS
+usuario_schema= UsuariosSchema()
+usuarios_schema= UsuariosSchema(many=True)
+    
+# ----------------------------------------------------------------------------------
+
+#FAMILIAS
+familia_schema = FamiliasSchema()
+familias_schema = FamiliasSchema(many=True)
+
+@app.route('/familias/ver', methods=['GET'])
+def ver_familia():
+    familias = Familias.query.all()
+    result = familias_schema.dump(familias)
+    return jsonify(result)
+
+@app.route('/familias/insertar', methods=['POST'])
+def insertar_familia():
+    nombre = request.json['nombre']
+
+    nueva_familia = Familias(nombre)
+
+    bd.session.add(nueva_familia)
+    bd.session.commit()
+
+    return familia_schema.jsonify(nueva_familia)
+
+@app.route('/familias/borrar/<id>', methods=['DELETE'])
+def borrar_familia(id):
+    familia = Familias.query.get(id)
+    bd.session.delete(familia)
+    bd.session.commit()
+
+    return familia_schema.jsonify(familia)
+
+@app.route('/familias/actualizar/<id>', methods=['PUT'])
+def actualizar_familia(id):
+    familia = Familias.query.get(id)
+    nombre = request.json['nombre']
+    familia.nombre = nombre
+    bd.session.commit()
+
+    return familia_schema.jsonify(familia)
+
+# ----------------------------------------------------------------------------------
+
+#CICLOS
+ciclo_schema = CiclosSchema()
+ciclos_schema = CiclosSchema(many=True)
+
+@app.route('/ciclos/ver', methods=['GET'])
+def ver_ciclo():
+    ciclos = Ciclos.query.all()
+    result = ciclos_schema.dump(ciclos)
+    return jsonify(result)
+
+@app.route('/ciclos/insertar', methods=['POST'])
+def insertar_ciclo():
+    id_ciclo = request.json['id_ciclo']
+    nombre = request.json['nombre']
+    real_decreto = request.json['real_decreto']
+    decreto = request.json['decreto']
+    id_familia = request.json['id_familia']
+
+    nuevo_ciclo = Ciclos(id_ciclo, nombre, real_decreto, decreto, id_familia)
+
+    bd.session.add(nuevo_ciclo)
+    bd.session.commit()
+
+    return ciclo_schema.jsonify(nuevo_ciclo)
+
+@app.route('/ciclos/borrar/<id>', methods=['DELETE'])
+def borrar_ciclo(id):
+    ciclo = Ciclos.query.get(id)
+    bd.session.delete(ciclo)
+    bd.session.commit()
+
+    return ciclo_schema.jsonify(ciclo)
+
+@app.route('/ciclos/actualizar/<id>', methods=['PUT'])
+def actualizar_ciclo(id):
+    ciclo = Ciclos.query.get(id)
+
+    id_ciclo = request.json['id_ciclo']
+    ciclo.id_ciclo = id_ciclo
+    nombre = request.json['nombre']
+    ciclo.nombre = nombre
+    real_decreto = request.json['real_decreto']
+    ciclo.real_decreto = real_decreto
+    decreto = request.json['decreto']
+    ciclo.decreto = decreto
+    id_familia = request.json['id_familia']
+    ciclo.id_familia = id_familia
+    
+    bd.session.commit()
+
+    return ciclo_schema.jsonify(ciclo)
+
+# ----------------------------------------------------------------------------------
+
+#CURSOS
+curso_schema = CursosSchema()
+cursos_schema = CursosSchema(many=True)
+
+@app.route('/cursos/ver', methods=['GET'])
+def ver_curso():
+    cursos = Cursos.query.all()
+    result = cursos_schema.dump(cursos)
+    return jsonify(result)
+
+@app.route('/cursos/insertar', methods=['POST'])
+def insertar_curso():
+    num_curso = request.json['num_curso']
+    id_ciclo = request.json['id_ciclo']
+
+    nuevo_curso = Cursos(num_curso, id_ciclo)
+
+    bd.session.add(nuevo_curso)
+    bd.session.commit()
+
+    return ciclo_schema.jsonify(nuevo_curso)
+
+@app.route('/cursos/borrar/<id>', methods=['DELETE'])
+def borrar_curso(id):
+    curso = Cursos.query.get(id)
+    bd.session.delete(curso)
+    bd.session.commit()
+
+    return curso_schema.jsonify(curso)
+
+@app.route('/cursos/actualizar/<id>', methods=['PUT'])
+def actualizar_curso(id):
+    curso = Cursos.query.get(id)
+
+    num_curso = request.json['num_curso']
+    curso.num_curso = num_curso
+    id_ciclo = request.json['id_ciclo']
+    curso.id_ciclo = id_ciclo
+    
+    bd.session.commit()
+
+    return ciclo_schema.jsonify(curso)
+
+# ----------------------------------------------------------------------------------
+
+#CENTROS
+centro_schema = CentrosSchema()
+centros_schema = CentrosSchema(many=True)
+
+@app.route('/centros/ver', methods=['GET'])
+def ver_centro():
+    centros = Centros.query.all()
+    result = centros_schema.dump(centros)
+    return jsonify(result)
+
+@app.route('/centros/insertar', methods=['POST'])
+def insertar_centro():
+    cod_centro = request.json['cod_centro']
+    nombre = request.json['nombre']
+    titularidad = request.json['titularidad']
+    direccion = request.json['direccion']
+    cp = request.json['cp']
+    localidad = request.json['localidad']
+    concejo = request.json['concejo']
+    provincia = request.json['provincia']
+    tlfno = request.json['tlfno']
+    fax = request.json['fax']
+    correo = request.json['correo']
+
+    nuevo_centro = Centros(cod_centro, nombre, titularidad, direccion, cp, localidad, concejo, provincia, tlfno, fax, correo)
+
+    bd.session.add(nuevo_centro)
+    bd.session.commit()
+
+    return centro_schema.jsonify(nuevo_centro)
+
+@app.route('/centros/borrar/<id>', methods=['DELETE'])
+def borrar_centro(id):
+    centro = Centros.query.get(id)
+    bd.session.delete(centro)
+    bd.session.commit()
+
+    return centro_schema.jsonify(centro)
+
+@app.route('/centros/actualizar/<id>', methods=['PUT'])
+def actualizar_centro(id):
+    centro = Centros.query.get(id)
+
+    cod_centro = request.json['cod_centro']
+    centro.cod_centro = cod_centro
+    nombre = request.json['nombre']
+    centro.nombre = nombre
+    titularidad = request.json['titularidad']
+    centro.titularidad = titularidad
+    direccion = request.json['direccion']
+    centro.direccion = direccion
+    cp = request.json['cp']
+    centro.cp = cp
+    localidad = request.json['localidad']
+    centro.localidad = localidad
+    concejo = request.json['concejo']
+    centro.concejo = concejo
+    provincia = request.json['provincia']
+    centro.provincia = provincia
+    tlfno = request.json['tlfno']
+    centro.tlfno = tlfno
+    fax = request.json['fax']
+    centro.fax = fax
+    correo = request.json['correo']
+    centro.correo = correo
+    
+    bd.session.commit()
+
+    return centro_schema.jsonify(centro)
+
+# ----------------------------------------------------------------------------------
+
+#CURSOS_HAS_CENTROS
+curso_has_centro_schema = Cursos_has_centrosSchema()
+cursos_has_centros_schema = Cursos_has_centrosSchema(many=True)
+
+@app.route('/cursos_has_centros/ver', methods=['GET'])
+def ver_cursos_has_centros():
+    cursos_has_centros = Cursos_has_centros.query.all()
+    result = cursos_has_centros_schema.dump(cursos_has_centros)
+    return jsonify(result)
+
+@app.route('/cursos_has_centros/insertar', methods=['POST'])
+def insertar_cursos_has_centros():
+    id_curso = request.json['id_curso']
+    cod_centro = request.json['cod_centro']
+
+    nuevo_curso_has_centro = Cursos_has_centros(id_curso, cod_centro)
+
+    bd.session.add(nuevo_curso_has_centro)
+    bd.session.commit()
+
+    return curso_has_centro_schema.jsonify(nuevo_curso_has_centro)
+
+#@app.route('/cursos_has_centros/borrar', methods=['DELETE'])
+#def borrar_cursos_has_centros():
+    id_curso = request.json['id_curso']
+    cod_centro = request.json['cod_centro']
+    print(id_curso, cod_centro)
+    cursos_has_centros = Cursos_has_centros.query(Cursos_has_centros).filter(Cursos_has_centros.id_curso == id_curso and Cursos_has_centros.cod_centro == cod_centro)
+    bd.session.delete(cursos_has_centros)
+    bd.session.commit()
+
+    return curso_has_centro_schema.jsonify(cursos_has_centros)
+
+#@app.route('/cursos_has_centros/actualizar/<id>', methods=['PUT'])
+#def actualizar_cursos_has_centros(id):
+    curso_has_centro = Cursos_has_centros.query.get(id)
+
+    id_curso = request.json['id_curso']
+    curso_has_centro.id_curso = id_curso
+    cod_centro = request.json['cod_centro']
+    curso_has_centro.cod_centro = cod_centro
+    
+    bd.session.commit()
+
+    return curso_has_centro_schema.jsonify(curso_has_centro)
+
+# ----------------------------------------------------------------------------------
+
+#MODULOS
+modulo_schema = ModulosSchema()
+modulos_schema = ModulosSchema(many=True)
+
+@app.route('/modulos/ver', methods=['GET'])
+def ver_modulo():
+    modulos = Modulos.query.all()
+    result = modulos_schema.dump(modulos)
+    return jsonify(result)
+
+@app.route('/modulos/insertar', methods=['POST'])
+def insertar_modulo():
+    cod_mod = request.json['cod_mod']
+    nombre = request.json['nombre']
+    nombre_abrev = request.json['nombre_abrev']
+    id_curso = request.json['id_curso']
+
+    nuevo_modulo = Modulos(cod_mod, nombre, nombre_abrev, id_curso)
+
+    bd.session.add(nuevo_modulo)
+    bd.session.commit()
+
+    return modulo_schema.jsonify(nuevo_modulo)
+
+@app.route('/modulos/borrar/<id>', methods=['DELETE'])
+def borrar_modulo(id):
+    modulo = Modulos.query.get(id)
+    bd.session.delete(modulo)
+    bd.session.commit()
+
+    return modulo_schema.jsonify(modulo)
+
+@app.route('/modulos/actualizar/<id>', methods=['PUT'])
+def actualizar_modulo(id):
+    modulo = Modulos.query.get(id)
+
+    cod_mod = request.json['cod_mod']
+    modulo.cod_mod = cod_mod
+    nombre = request.json['nombre']
+    modulo.nombre = nombre
+    nombre_abrev = request.json['nombre_abrev']
+    modulo.nombre_abrev = nombre_abrev
+    id_curso = request.json['id_curso']
+    modulo.id_curso = id_curso
+    
+    bd.session.commit()
+
+    return modulo_schema.jsonify(modulo)
+
+# ----------------------------------------------------------------------------------
+
+#REGIMENES
+regimen_schema = RegimenSchema()
+regimenes_schema = RegimenSchema(many=True)
+
+@app.route('/regimenes/ver', methods=['GET'])
+def ver_regimen():
+    regimenes = Regimen.query.all()
+    result = regimenes_schema.dump(regimenes)
+    return jsonify(result)
+    
+# ----------------------------------------------------------------------------------
+
+#VIAS ACCESO
+via_acceso_schema = Vias_accesoSchema()
+vias_acceso_schema = Vias_accesoSchema(many=True)
+
+@app.route('/vias_acceso/ver', methods=['GET'])
+def ver_via_acceso():
+    vias_acceso = Vias_acceso.query.all()
+    result = vias_acceso_schema.dump(vias_acceso)
+    return jsonify(result)
+
+# ----------------------------------------------------------------------------------
+
+#SESIONES
+sesion_schema = SesionesSchema()
+sesiones_schema = SesionesSchema(many=True)
+
+#@app.route('/sesiones/ver', methods=['GET'])
+#def ver_sesiones():
+    #sesiones = Sesiones.query.all()
+    #result = sesiones_schema.dump(sesiones)
+    #return jsonify(result)
+
+# ----------------------------------------------------------------------------------
+
+#FECHAS EVALUACIÓN
+fecha_evaluacion_schema = Fechas_evaluacionSchema()
+fechas_evaluacion_schema = Fechas_evaluacionSchema(many=True)
+
+@app.route('/fechas_evaluacion/ver', methods=['GET'])
+def ver_fechas_evaluacion():
+    fechas_evaluacion = Fechas_evaluacion.query.all()
+    result = fechas_evaluacion_schema.dump(fechas_evaluacion)
+    return jsonify(result)
+
+@app.route('/fechas_evaluacion/insertar', methods=['POST'])
+def insertar_fecha_evaluacion():
+    cod_sesion = request.json['cod_sesion']
+    fecha_curso = request.json['fecha_curso']
+    fecha_eval = request.json['fecha_eval']
+    id_sesion = request.json['id_sesion']
+
+    nueva_fecha_evaluacion = Fechas_evaluacion(cod_sesion, fecha_curso, fecha_eval, id_sesion)
+
+    bd.session.add(nueva_fecha_evaluacion)
+    bd.session.commit()
+
+    return fecha_evaluacion_schema.jsonify(nueva_fecha_evaluacion)
+
+@app.route('/fechas_evaluacion/borrar/<id>', methods=['DELETE'])
+def borrar_fecha_evaluacion(id):
+    fecha_evaluacion = Fechas_evaluacion.query.get(id)
+    bd.session.delete(fecha_evaluacion)
+    bd.session.commit()
+
+    return fecha_evaluacion_schema.jsonify(fecha_evaluacion)
+
+@app.route('/fechas_evaluacion/actualizar/<id>', methods=['PUT'])
+def actualizar_fecha_evaluacion(id):
+    fecha_evaluacion = Fechas_evaluacion.query.get(id)
+
+    cod_sesion = request.json['cod_sesion']
+    fecha_evaluacion.cod_sesion = cod_sesion
+    fecha_curso = request.json['fecha_curso']
+    fecha_evaluacion.fecha_curso = fecha_curso
+    fecha_eval = request.json['fecha_eval']
+    fecha_evaluacion.fecha_eval = fecha_eval
+    id_sesion = request.json['id_sesion']
+    fecha_evaluacion.id_sesion = id_sesion
+    
+    bd.session.commit()
+
+    return fecha_evaluacion_schema.jsonify(fecha_evaluacion)
+
+# ----------------------------------------------------------------------------------
+
+#MATRICULACIONES
+matricula_schema = MatriculacionesSchema()
+matriculaciones_schema = MatriculacionesSchema(many=True)
+
+#@app.route('/matriculaciones/ver', methods=['GET'])
+#def ver_matriculaciones():
+    #matriculaciones = Matriculaciones.query.all()
+    #result = matriculaciones_schema.dump(matriculaciones)
+    #return jsonify(result)
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/')
+def hello():
+    return 'Hola holita vecinitos'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
